c956e58c9e28fe739649029a8cae5d7aad87985e
Maciej Bogusz <maciekbogusz@yahoo.com>
2017-03-14 15:29:29 +0100

    new structure of database

--- src/queries.sql
+++ src/queries.sql
@@ -1,25 +1,32 @@
-CREATE TABLE  family_member(
-	id 	BIGSERIAL PRIMARY KEY,
-	name 	varchar(30) not null,
-	surname varchar(60) not null,
-	birht_date date,
-	death_date date
+create table individuals(
+	id serial primary key,
+	first_name varchar(25),
+	last_name varchar(40),
+	date_of_birth date,
+	date_of_death date
 );
 
-CREATE TABLE  parents(
-	id 	BIGSERIAL PRIMARY KEY,
-	name 	varchar(30) not null,
-	surname varchar(60) not null,
-	child_id BIGSERIAL references family_member(id),
-	birht_date date,
-	death_date date
+create table relationships_types(
+	relationship_type_code serial primary key,
+	relationship_type_description varchar(40)
 );
+
+create table roles(
+	role_code serial primary key,
+	role_description varchar(40)
+);
+
+create table relationships(
+	relationship_id serial primary key,
+	individual_id serial references individuals(id),
+	releationship_type_code serial references relationships_types(relationship_type_code),
+	individual_1_role_code serial references roles(role_code)
+);
+
 
 CREATE SEQUENCE primary_key_seq 
 	start 1
 	increment 1
 	NO MAXVALUE
 	CACHE 1;
-
-
 
